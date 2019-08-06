7b8a5ce53773e9f0b40e0901f7c3ccb65ccb9875
bandon <2644010899@qq.com>
2019-08-06 21:55:09 +0800

    张三修改

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("再次提交");
-        System.out.println("张三修改");
+        System.out.println("张三修改123");
+
     }
 }
